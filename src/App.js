c52cf8844fd4085b317d35ec01b4e4b3caf364ff
mcstagg <mcstagg@madisoncollege.edu>
2021-02-15 21:38:40 -0600

    We saw our first React props...

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { Greeting } from './Greeting';
 const App = () => {
   return (
     <>
-    <Greeting />
+    <Greeting name="Mike"/>
     <h1>
       Hello from React !!!!!!!!!!
     </h1>
     <h2>This shit is declicious!</h2>
     <h2>How did she get there?!</h2>
-    <Greeting />
-    <Greeting />
-    <Greeting />
+    <Greeting name="Tom"/>
+    <Greeting name="Mark"/>
+    <Greeting name="Travis"/>
     </>
   );
 }
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export const Greeting = () => {
+export const Greeting = (props) => {
+
+    console.log(props);
+
     return (
         <h2>
-            Peace Among Worlds !!!
+            Peace Among Worlds {props.name} !!!
         </h2>
     );
 };
